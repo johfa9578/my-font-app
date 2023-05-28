187f99b12e1b56e1e9e8806ee52f750666e7653d
Johanna Fagerås <heyjohfa@gmail.com>
2023-05-28 21:16:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ exports.apiKey = 'nf1nkug1cgls1c0uvervcbidtgvxto5wulhzflc94087w508';
 exports.privateKeyFile = '/etc/secrets/private.key';
 
 exports.users = [
-  { username: 'Johanna', password: 'Markus1982!!!!!', fullname: 'Johanna Fagerås' },
-  { username: 'Guest', password: 'Guest', fullname: 'Guest User' }
+  { username: 'Johanna', password: 'Markus1982!!!!!', firstname: 'Johanna', lastname: 'Fagerås', fullname: 'Johanna Fagerås' },
+  { username: 'Guest', password: 'Guest', firstname: 'Guest', lastname: 'User', fullname: 'Guest User' }
 ];
 
 exports.scopeUser = true;
