ae02031fa1295f2409786e43d96eaf379ca34694
Johanna Fagerås <heyjohfa@gmail.com>
2023-05-28 08:54:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
-// Replace this with your api key from the "API Key Manager" at the tiny.cloud account page
 exports.apiKey = 'nf1nkug1cgls1c0uvervcbidtgvxto5wulhzflc94087w508';
 
-// Replace the contents of the private.key file with the one from the "JWT Key Manager" at the tiny.cloud account page
 exports.privateKeyFile = '/etc/secrets/private.key';
 
-// This is the fake database that the login authenticates against
 exports.users = [
-  { username: 'Johanna', password: 'Markus1982!!!!!', fullname: 'Johanna' },
-  { username: 'Guest', password: 'Guest', fullname: 'Guest' }
+  { username: 'Johanna', password: 'Markus1982!!!!!', fullname: 'Johanna Fagerås' },
+  { username: 'Guest', password: 'Guest', fullname: 'Guest User' }
 ];
 
-// If this is enabled the root of Tiny Drive will be within a directory named as the user login
-exports.scopeUser = false;
+exports.scopeUser = true;
